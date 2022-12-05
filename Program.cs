6310e4c7ebf9aeada65382a9f472fac73d373d2a
Katya <www@mail.ru>
2022-12-05 11:58:24 +0300

    OK

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Task 4
+ int i = 2;
+ Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Чётные числа от 1 до " + num);
+     while (i <= num)
+         {
+            if (i % 2 == 0)
+             {
+                    Console.Write(i + ", ");
+                
+             }
+             i++;
+             
+            
+        } 
+                
+             
+           
+    
